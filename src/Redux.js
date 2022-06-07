166a0c8db8e464d85599c7d777e6b59bc108b466
ramadhanbridge <nyiringondoramadan@gmail.com>
2022-06-07 10:19:37 +0200

    reducer

--- src/Redux.js
+++ src/Redux.js
@@ -1,1 +1,25 @@
-console.log("hello")
+const CAKE_ORDERED = "CAKE_ORDERED"
+let initialState = {
+    numOfCakes: 10
+}
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+
+
+
